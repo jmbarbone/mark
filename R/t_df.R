8d6d25f9ecd6f015fe0986d3ad85432722adee26
jordan.barbone <jordan.barbone@bracketglobal.com>
2019-06-17 21:03:01 -0400

    transpose to data.frame
    function that enhances `t()` to provide back a data.frame with a selected `.id` column

---
+++ R/t_df.R
@@ -0,0 +1,8 @@
+t_df <- function(.data.frame, .id = 1)
+{
+  temp <- data.frame(t(.data.frame[-.id]), stringsAsFactors = F, row.names = NULL)
+  colnames(temp) <- unlist(.data.frame[,.id], use.names = F)
+  temp$.id <- colnames(.data.frame)[-.id]
+  n <- ncol(temp)
+  temp[c(".id", colnames(temp)[-n])]
+}
