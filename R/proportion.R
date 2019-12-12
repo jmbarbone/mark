ce7f0d6f9f3171c3a24e52c67e6eab5d7e59e4e7
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2019-12-12 16:44:27 -0500

    proportions
    update of proportions to pass testing

--- R/proportion.R
+++ R/proportion.R
@@ -4,6 +4,10 @@
 #'
 #' @param x A data.frame or a vector.
 #' @param ... Additional arguments to be passed to methods.
+#'
+#' @examples
+#' proportion(iris, Species)
+#'
 #' @import stats
 #' @export
 
@@ -20,17 +24,27 @@ proportion.tbl <- function(x, column) {
   tibble::enframe(props, col, "prop")
 }
 
-proportion.data.frame <- function(x, column) {
-  col <- as.character(substitute(column))
+proportion.data.frame <- function(x, col) {
 
-  groups <- .my_sort(x[[col]])
-  props <- vapply(groups, function(x) mean(x[[col]]), double(1), USE.NAMES = F)
+  if(is.name(substitute(col))) {
+    vals <- eval(substitute(col), x, enclos = parent.frame())
+    col_name <- deparse(substitute(col))
+  } else {
+    vals <- x[[col]]
+    col_name <- col
+  }
 
-  data.frame(col = groups,
-             prop = props)
+  groups <- unique(vals)
+
+  res <- data.frame(
+    col_name = groups,
+    props = vapply(groups, function(.x) mean(.x == vals), double(1))
+  )
+
+  names(res)[1] <- col_name
+  res
 }
 
 .my_sort <- function(x) UseMethod(".my_sort")
 .my_sort.factor <- function(x) unique(x)
-.my_sort.character <- function(x) sort(unique(x))
-.my_sort.numeric <- function(x) sort(unique(x))
+.my_sort.default <- function(x) sort(unique(x))
