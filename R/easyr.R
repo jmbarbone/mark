ecf5741b26da240db5f1404bb86bab12d0cd9980
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2019-12-13 14:27:47 -0500

    openxlsx-utils
    inclusion of wrappers for openxlsx

---
+++ R/easyr.R
@@ -0,0 +1,7 @@
+#' Easyr helpers and wrappers
+#'
+#' Additional functions and wrappers to make my day (and possibly others') easier.
+#'
+#' @docType package
+#' @name easyr
+"_PACKAGE"
---
+++ R/openxlsx-utils.R
@@ -0,0 +1,65 @@
+#' Wrappers for openxlsx pacakge functions
+#'
+#' Add data to sheet
+#'
+#' A wrapper function to use within the openxlsx workbook building.
+#' This adds additional functionality to override within the function and provude an output that can be piped.
+#'
+#' @details These should be applied to a string starting with `openxlsx::createWorkbook()` then piped through with the functions below.
+#'
+#' @param wb A Workbook object to attach the new worksheet and table
+#' @param data A dataframe.
+#' @param sheetname The worksheet to write to. Can be the worksheet index or name.
+#' @param tableStyle Any excel table style name or "none".
+#' @param bandedRows Logical. If TRUE, rows are colour banded
+#' @param bandedCols Logical. If TRUE, the columns are colour banded
+#' @param ... Additional arguments passed to openxlsx::writeDataTable that are not already listed
+#' @param override Logical.  If TRUE, will delete the sheetname (if present)
+#'
+#' @importFrom openxlsx removeWorksheet
+#' @importFrom openxlsx addWorksheet
+#' @importFrom openxlsx writeDataTable
+#'
+#'
+#' @export
+
+add_data_sheet <- function(wb, data, sheetname,
+                           tableStyle = "TableStyleLight8", bandedRows = TRUE, bandedCols = TRUE, ...,
+                           override = T)
+{
+  requireNamespace("openxlsx", quietly = TRUE)
+
+  if(override && sheetname %in% wb$sheet_names) removeWorksheet(wb, sheetname)
+
+  addWorksheet(wb, sheetname)
+  writeDataTable(wb, sheetname, x = data,
+                 tableStyle = tableStyle,
+                 bandedRows = bandedRows,
+                 bandedCols = bandedCols,
+                 ...)
+  invisible(wb)
+}
+
+#' Add image to sheet
+#'
+#' A wrapper function to..
+#'
+#' @param wb A Workbook object to attach the new worksheet and table
+#' @param file An image file. Valid file types are: jpeg, png, bmp
+#' @param sheetname The worksheet to write to. Can be the worksheet index or name.
+#' @param ... Additional arguments passed to openxlsx::insertImage
+#' @param override Logical.  If TRUE, will delete the sheetname (if present)
+#'
+#' @importFrom openxlsx removeWorksheet
+#' @importFrom openxlsx addWorksheet
+#' @importFrom openxlsx insertImage
+#'
+#' @export
+
+add_image_sheet <- function(wb, file, sheetname, ..., override = T)
+{
+  if(override && sheetname %in% wb$sheet_names) removeWorksheet(wb, sheetname)
+  addWorksheet(wb, sheetname)
+  insertImage(wb, sheetname, file = file, ...)
+  invisible(wb)
+}
