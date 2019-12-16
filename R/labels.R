05ae3421440423da59534db35d9d356ad3a98665
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2019-12-16 14:12:21 -0500

    assign_labels
    Initial commit -- function to assign labels to a dataset

---
+++ R/labels.R
@@ -0,0 +1,25 @@
+#' Dataframe labels
+#'
+#' Assign labels to data frames
+#'
+#' @param .data A dataframe
+#' @param ... One or more unquoted expressed separated by commas
+#'
+#' @importFrom Hmisc label
+#' @export
+#'
+#' @example
+#' ## Best when run with RStudio
+#' labs <- assign_label(iris, Sepal.Length = "cms", Sepal.Width = "cms", Petal.Length = "cms", Petal.Width = "cms", Species = "Iris ...")
+#' View(labs)
+
+assign_label <- function(.data, ...) {
+  ls <- list(...)
+  n <- names(ls)
+
+  for(i in seq_along(n)) {
+    Hmisc::label(.data[[n[i]]]) <- ls[[i]]
+  }
+
+  .data
+}
