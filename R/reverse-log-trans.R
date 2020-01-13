9684f8cf2f71cf081fcb8d3a4cbeaf99e70b1f3a
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-01-13 09:49:15 -0500

    Create reverse-log-trans
    Initial commit

---
+++ R/reverse-log-trans.R
@@ -0,0 +1,20 @@
+#' Reverse log transformation for ggplot
+#'
+#' Show a reversed order for a log transformed scale from ggplot.
+#' Ripped from: https://stackoverflow.com/questions/11053899/how-to-get-a-reversed-log10-scale-in-ggplot2
+#'
+#' @inheritParams base::log
+#'
+#' @author Brian Diggs (https://stackoverflow.com/users/892313/brian-diggs)
+#' @importFrom scales trans_new
+#' @importFrom scales log_breaks
+#'
+#' @export
+
+reverse_log_trans <- function(base = exp(1)) {
+  scales::trans_new(name      = paste0("reverselog-", format(base)),
+                    transform = function(x) -log(x, base),
+                    inverse   = function(x) base^(-x),
+                    breaks    = scales::log_breaks(base = base),
+                    domain    = c(1e-100, Inf))
+}
