304d10f09cb8c34922b3c77a6380efc87c85f09b
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-01-14 14:26:32 -0500

    removed stringr imports

--- R/str-utils.R
+++ R/str-utils.R
@@ -6,17 +6,19 @@
 #' @param string Input vector. Either a character vector, or something coercible to one.
 #' @param pattern A character string of the actual value (see below)
 #' @param negate If TRUE, return non-matching elements.
-#' @param ... Additional arguments passed to `stringr::regex()`
 #'
 #' @details pattern will be coerced like so: "abc" turned into "[a|b|c][a|b|c][a|b|c]"
 #'
-#' @importFrom stringr regex
-#' @importFrom stringr str_detect
 #' @export
+#'
+#' @examples
+#' str_close_enough(c("thsi", "TIHS", "that"), "this", negate = TRUE)  ##  TRUE  TRUE FALSE
+#' str_close_enough(c("thsi", "TIHS", "that"), "this", negate = FALSE) ## FALSE FALSE  TRUE
 
-str_close_enough <- function(string, pattern, ..., negate = FALSE) {
+str_close_enough <- function(string, pattern, negate = FALSE) {
   temp <- unlist(strsplit(pattern, ""))
   unit <- paste(temp, collapse = "|")
   find <- paste0("[", paste(rep(unit, length(temp)), collapse = "]["), "]")
-  stringr::str_detect(string, stringr::regex(find, ignore_case = T, ...), negate = negate)
-}
+  res <- grepl(pattern = find, x = string, ignore.case = TRUE)
+  if(negate) return(.Primitive("!")(res)) else res
+  }
