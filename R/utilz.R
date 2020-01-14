4144fe606e85c0bcc157d394f71513916b99b16e
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-01-14 15:27:14 -0500

    Update utilz.R
    add `require_namespace()`

--- R/utilz.R
+++ R/utilz.R
@@ -4,3 +4,12 @@ deparser <- function(x, env = parent.frame()) {
   if(class(substitute(x, env)) == "name") deparse(substitute(x, env)) else x
 }
 
+
+# Happily ripped from: http://r-pkgs.had.co.nz/description.html
+
+require_namespace <- function(namespace) {
+  if (!requireNamespace(namespace, quietly = TRUE)) {
+    stop(sprintf("Package \"%s\" needed for this function to work. Please install it.", namespace),
+         call. = FALSE)
+  }
+}
