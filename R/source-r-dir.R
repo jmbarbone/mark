334d8979a2855fe5c413bd7ecf320fb26a288934
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-01-14 15:39:00 -0500

    removed purrr dependency

--- R/source-r-dir.R
+++ R/source-r-dir.R
@@ -4,19 +4,20 @@
 #'
 #' @param dir The location of your R scripts
 #' @param quiet Logical.  Whether to print out a message for each file.
+#' @inheritParams base::source
+#' @param ... Additional arguments passed to [base::source()]
 #'
-#' @importFrom purrr map
 #' @examples
 #' \dontrun{
-#' source_dir_r("C:/Users/jbarbone/GitHub/easyr/R/")
+#' source_dir_r("C:/Users/jbarbone/GitHub/test")
 #' }
 #' @export
 
-source_dir_r <- function(dir, quiet = F) {
-  invisible(purrr::map(list.files(dir, pattern = "\\.R$", full.names = T),
-                       function(x, q) {
-                         source(x, echo = F)
-                         if(!q) message(sprintf("Successfully sourced: %s", x))
-                         invisible()},
-                       q = quiet))
+source_dir_r <- function(dir, echo = FALSE, quiet = FALSE, ...) {
+  invisible(lapply(list.files(dir, pattern = "\\.R$", full.names = T),
+                   function(x, q) {
+                     source(x, echo = echo, ...)
+                     if(!q) message(sprintf("Successfully sourced: %s", x))
+                     invisible()},
+                   q = quiet))
 }
