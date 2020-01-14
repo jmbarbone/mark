b7af2206aead529b5726134de0ec6923120e6dbb
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-01-14 15:40:43 -0500

    knitr imports to suggests
    single function which uses knitr::purl

--- R/ksource.R
+++ R/ksource.R
@@ -3,12 +3,12 @@
 #' Sources an Rmd file as a temp file
 #'
 #' @param x An Rmd file.
-#' @param ... Additional arguments passes to `knitr::purl`
+#' @param ... Additional arguments passes to [knitr::purl()]
 #' @param quiet Logical.  Determines whether to apply silence to `knitr::purl`
-#' @import knitr
 #' @export
 
 ksource <- function(x, ..., quiet = TRUE)
 {
+  require_namespace("knitr")
   source(knitr::purl(x, output = tempfile(), quiet = quiet, ...))
 }
