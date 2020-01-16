93685e33fc82f1799e3afcbace82e8c59779892f
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-01-16 16:22:35 -0500

    Update utilz.R
    add construct_date and date_switch functions for testing

--- R/utilz.R
+++ R/utilz.R
@@ -22,3 +22,24 @@ all_na <- function(x) {
   all(is.nan(x) || is.na(x))
 }
 # all_na(c(NA, NaN, NA_character_, NA_complex_))
+
+
+construct_date <- function(date_text, collapse = "") {
+  paste(sapply(unlist(strsplit(date_text, "")), date_switch, USE.NAMES = FALSE), collapse = collapse)
+}
+
+date_switch <- function(x) {
+  switch(x,
+         Y = "[[:digit:]]{4}",
+         y = "[0-99]{2}",
+         m = "1[0-2]|[1-9]  ",
+         b = "[[:alpha:]]{3,}",
+         d = "3[01]|[12][0-9]|[1-9]",
+         NULL)
+}
+
+# construct_date("ymd")
+# construct_date("bdY")
+# grepl(construct_date("bdY", "[\\s|,]+"), "December 17, 1992", ignore.case = TRUE)
+# grepl(construct_date("Ymd", "-"), "1992-12-17", ignore.case = TRUE)
+
