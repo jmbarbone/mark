dc7a9bab3594a0bfeb15b44abb4057377276e8cf
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-01-20 12:29:05 -0500

    test-out update
    + simply %in% %out% test
    + better spacing
    + correct all_na to use separate  character method (NaN overriden to "NaN" and not registered as is.nan()

--- R/utilz.R
+++ R/utilz.R
@@ -22,10 +22,31 @@ opposite <- function(x, y) {
   all(x != y)
 }
 
-all_na <- function(x) {
-  all(is.nan(x) || is.na(x))
+all_na <- function(x, ...) {
+  UseMethod("all_na", x)
 }
-# all_na(c(NA, NaN, NA_character_, NA_complex_))
+
+all_na.default <- function(x) {
+  all(is.nan(x) | is.na(x))
+}
+
+all_na.character <- function(x, convert = FALSE) {
+  found <- which(x == "NaN")
+  if(convert) {
+    x[found] <- NA_character_
+  } else if(length(found) > 0) {
+    warning("These values may be NA types `convert`ed to character:\n",
+            paste(paste0("    ",  x[found]), collapse = "\n"),
+            call. = FALSE)
+  }
+  all_na.default(x)
+}
+
+# all_na(c(NA, NaN, NA_complex_, NA_integer_))
+# all_na(c(NA, NA_character_))
+# all_na(c(NA, NA_character_, NA_complex_, NA_integer_))
+# all_na(c(NA_character_, NaN)) ## fails
+# all_na(c(NA_character_, NaN), override_nan = TRUE)
 
 
 construct_date <- function(date_text, collapse = "") {
--- tests/testthat/test-out.R
+++ tests/testthat/test-out.R
@@ -1,10 +1,9 @@
 context("Testing out functionality")
 
 test_that("`%out%` returns opposite of `%in%`", {
-
-    # examples from `%in%`
-  expect_true(opposite(1:10 %in%  c(1,3,5,9),
-                       1:10 %out% c(1,3,5,9)))
+  a <- 1:10
+  b <- c(1, 3, 5, 9)
+  expect_true(all(a %in% b != a %out% b))
 
   sstr <- c("c","ab","B","bba","c",NA,"@","bla","a","Ba","%")
   expect_true(no_match(sstr[sstr %in%  c(letters, LETTERS)],
@@ -13,10 +12,11 @@ test_that("`%out%` returns opposite of `%in%`", {
 })
 
 test_that("`%w/o%` returns duplicates.", {
-  expect_equal(c(1:6,7:2) %w/o% c(3,7,12),
+  expect_equal(c(1:6, 7:2) %w/o% c(3, 7, 12),
                c(1, 2, 4, 5, 6, 6, 5, 4, 2))
 })
 
 test_that("all_na() returns correctly from example,", {
-  expect_true(all_na(c(NA, NaN, NA_character_, NA_complex_)))
+  expect_true(all_na(c(NA, NaN, NA_character_, NA_complex_), convert = TRUE))
 })
+
