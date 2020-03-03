db6c26ff0813f55b26efe86bdc4c06194c560d63
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-03-03 10:52:01 -0500

    updated `between_more()` documentation
    + replaces vector check with `match.arg(type)`
    + replaces `F` with `FALSE`
    + adds itemized documentation for `type` parameter
    + updates documentation for `type` parameter

--- R/between_more.R
+++ R/between_more.R
@@ -4,16 +4,27 @@
 #'
 #' @param x A numeric vector of values.
 #' @param left,right Boundary values.
-#' @param type Abrreviated type (g: greater than, ge: greater than or equal to, l: less than, le: less than or equal to)
+#' @param type Abbreviation for the evaluation of `left` on `right`
+#'
+#' @details
+#' Type can be one of the below:
+#' \itemize{
+#'  \item{"\strong{g}": }{is greater than (>)}
+#'  \item{"\strong{ge}": }{greater than or equal to (>=)}
+#'  \item{"\strong{l}": }{less than (<)}
+#'  \item{"\strong{le}": }{less than or equal to (<=)}
+#' }
+#'
 #' @export
-#' @seealso `dplyr::case_when()`
+#'
+#' @seealso [dplyr::case_when()]
 #' @examples
 #' between_more(10, 2, 10, "gl")
 #' between_more(10, 2, 10, "gle")
 
-between_more <- function(x, left, right, type = "gele") {
-  if(!type %in% c("gele", "gel", "gle", "gl")) stop("Not a valid type!", call. = F)
-  if(left > right) warning("`left` > `right`", call. = F)
+between_more <- function(x, left, right, type = c("gele", "gel", "gle", "gl")) {
+  type <- match.arg(type)
+  if(left > right) warning("`left` > `right`", call. = FALSE)
 
   vapply(x, function(x) {
     switch(type,
