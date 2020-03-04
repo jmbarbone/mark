6e24e5f5db49b60856651ee490ab1f0716dada19
jmbarbone <jmbarbone@gmail.com>
2020-03-03 21:45:43 -0500

    updates list2df
    + adds s3 classes of default (provides error) and list
    + adds params for key and value names
    + removes unused dot params
    + cleans up some documentation
    + adds example

--- R/list2df.R
+++ R/list2df.R
@@ -2,25 +2,59 @@
 #'
 #' Converts a list object into a a tidy data.frame
 #'
-#' @param .list A (prefferably) named `list` with any number of values
-#' @param show_NA Logical.  If FALSE elements without names will be listed as ""; otherwise as NA.
-#' @param ... Arguments passed to `as.data.frame()`
-#' @return a `data.frame` object with columns "name" and "value" for the names of the `list` and the values in each
+#' @param list A (preferably) named `list` with any number of values
+#' @param key,value Names of the new key and value columns, respectively
+#' @param show_NA Logical; if FALSE elements without names will be listed as ""; otherwise as NA
+#' @return a `data.frame` object with columns "name" and "value" for the names of the `list`
+#'   and the values in each
 #' @export
+#' @examples
+#' \dontrun{
+#' list2df(list(a = 1, b = 2:4, c = letters[10:20]), "col1", "col2")
+#' }
 
-list2df <- function(.list, show_NA = F, ...)
-{
-  if(!is.list(.list)) stop("Object is not a list!", call. = F)
-  classes <- sapply(.list, class, simplify = T)
-  n_classes <- length(unique(classes))
-  if(n_classes > 1) warning("Not all values are the same class!", call. = F)
-  if(n_classes > 1 && "character" %in% classes) warning("Values converted to character.", call. = F)
-
-  list_names <- rep(names(.list), sapply(.list, length))
-  if(show_NA) list_names[list_names == ""] <- NA else if(is.null(list_names) || any(list_names == "")) warning("Name missing from list.", call. = F)
-
-  data.frame(
-    name = list_names,
-    value = unlist(.list, use.names = F),
-    stringsAsFactors = F)
+list2df <- function(list, key = "key", value = "value", show_NA = FALSE) {
+  UseMethod("list2df", list)
 }
+
+#' @export
+list2df.default <- function(list, key = "key", value = "value", show_NA = FALSE) {
+  stop("Object must be class list", call. = FALSE)
+}
+
+#' @export
+list2df.list <- function(list, key = "key", value = "value", show_NA = FALSE) {
+  classes <- sapply(list, class, simplify = TRUE)
+  n_classes <- length(unique(classes))
+
+  if(n_classes > 1) {
+    warning("Not all values are the same class", call. = FALSE)
+    if(any(c("character", "factor") %in% classes)) {
+      warning("Values converted to character", call. = FALSE)
+    }
+  }
+
+  list_names <- rep(names(list), sapply(list, length))
+
+  if(show_NA) {
+    list_names[list_names == ""] <- NA
+  }  else if(is.null(list_names) | any(list_names == "")) {
+    warning("Name missing from list.", call. = FALSE)
+  }
+
+  data.frame(key = list_names,
+             value = unlist(list, use.names = FALSE),
+             stringsAsFactors = FALSE) %>%
+    `names<-`(c(key, value))
+}
+
+
+# testing -------------------------------------------------------------------------------------
+
+# length(x)
+# lengths(x)
+#
+# for(i in seq_along(x)) {
+#   magrittr::extract2(x, 1)
+#   print(sapply(x[[i]], length))
+# }
