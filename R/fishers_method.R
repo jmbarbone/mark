a3fe2408355d820d700d8e07ffa44f2cb991f384
jmbarbone <jmbarbone@gmail.com>
2020-04-13 14:38:44 -0400

    reduces objects saved
    + this function will be moved to a separate statistical package

--- R/fishers_method.R
+++ R/fishers_method.R
@@ -1,17 +1,16 @@
-#' Fisher's method for combined probabilites
+#' Fisher's method for combined probabilities
 #'
 #' Computes Fisher's method for combined probabilities
 #'
-#' @param .x A vector of p-values.
-#' @param ... Additional arguments passed to `base:pchisq`
+#' @param x A vector of p-values.
 #' @importFrom stats pchisq
 #' @export
 
-fishers_method <- function(.x, ...) {
-  valid <- .x[.x <= 1 & .x >= 0]
+fishers_method <- function(x) {
+  valid <- x[x <= 1 & x >= 0]
   ps <- valid[valid != 0]
   ps[ps == 0] <- min(valid)
-  log_ps <- log(ps)
-  estimate <- (-2) * sum(log_ps)
-  pchisq(estimate, df = 2 * length(log_ps), lower.tail = F, ...)
+  pchisq((-2) * sum(log(ps)),
+         df = 2 * length(ps),
+         lower.tail = FALSE)
 }
