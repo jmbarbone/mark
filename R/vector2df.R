c4090b481bed5cbbb25d844b9966d5c21338bd5f
jmbarbone <jmbarbone@gmail.com>
2020-04-13 14:38:15 -0400

    better formatting

--- R/vector2df.R
+++ R/vector2df.R
@@ -8,13 +8,16 @@
 
 vector2df <- function(.vector, show_NA = F)
 {
-  if(!is.vector(.vector)) stop("Object is not a vector!", call. = F)
+  if(!is.vector(.vector)) stop("Object is not a vector.", call. = FALSE)
   vector_names <- names(.vector)
-  if(show_NA) vector_names[vector_names == ""] <- NA else if(is.null(vector_names) || any(vector_names == "")) warning("Name missing from vector.", call. = F)
-
+  if(show_NA) {
+    vector_names[vector_names == ""] <- NA
+  } else if(is.null(vector_names) || any(vector_names == "")) {
+    warning("Name missing from vector.", call. = FALSE)
+  }
   data.frame(
     names = vector_names,
     values = unname(.vector),
-    stringsAsFactors = F
+    stringsAsFactors = FALSE
   )
 }
