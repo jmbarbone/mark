f279079c248424bf03ca31673c7d133d6075dfc4
jmbarbone <jmbarbone@gmail.com>
2020-04-18 15:44:09 -0400

    updates percentile rank and proportions (match from qpm)

--- R/percentile-rank.R
+++ R/percentile-rank.R
@@ -1,14 +1,14 @@
 #' Percentile rank
 #'
 #' Computes a percentile rank for each score in a set.
-#' The bounds of a percentile rank are 0 - 99th percentile.
+#' The bounds of a percentile rank are > 0 and < 100.
 #' A percentile rank here is the proportion of scores that are less than
 #'   the current score.
 #'
 #' @details
 #' This is not a very fast formula, but it is correct.
 #'
-#' \deqn{PR = (c_L + 0.5 f_i) / N}
+#' \deqn{PR = (c_L + 0.5 f_i) / N * 100}
 #'
 #' Where
 #'
@@ -18,15 +18,18 @@
 #'
 #'
 #' @param x A vector of values to rank
+#' @export
+#'
+#' @return A percentile rank between 0L and 100L
 #'
 #' @examples
 #' x <- c(1, 1, 2, 5, 7, 7, 8, 10)
 #' percentile_rank(x)
 #' \dontrun{
-#' dplyr::percent_rank(x)
+#' dplyr::percent_rank(x) * 100
 #' }
 
 percentile_rank <- function(x) {
-  p <- unname(proportion(x))
-  (cumsum(p) - p + p * .5)[match(x, sort(unique(x)))]
+  p <- unname(prop_tapply(x))
+  (cumsum(p) - p + p * .5)[match(x, sort(unique(x)))] * 100
 }
--- R/proportion.R
+++ R/proportion.R
@@ -1,59 +1,105 @@
-#' Calculate a proportion
+#' Proportions
 #'
-#' Calculates a proportion!
+#' Calculates a proportion from a vector or data.frame/matrix.
+#'
+#' @details
+#' tapply(x, x, length) / length(x) can be speedier for smaller data sets and groups
+#'   but slows down with more data or a greater number of groups.
 #'
 #' @param x A data.frame or a vector.
-#' @param .name The name of the column or new name for the group.  For data.frames this must be set.
+#' @param ... Additional arguments to be passed to methods.
 #'
 #' @examples
-#' proportion(iris, Species)
-#' proportion(iris$Species)
+#' proportion(iris, "Species")
 #' proportion(iris$Species)
 #'
+#'\dontrun{
+#' ## With smaller n, tapply edges out as faster
+#' microbenchmark::microbenchmark(
+#'   tapply = prop_tapply(iris, "Sepal.Length"),
+#'   proportion = proportion(iris, "Sepal.Length"),
+#'   times = 1000
+#' )
+#' microbenchmark::microbenchmark(
+#'   tapply = prop_tapply(iris$Species),
+#'   proportion = proportion(iris$Species),
+#'   times = 1000
+#' )
+#'
+#' ## But in larger n, proportion is faster
+#' x <- dplyr::sample_n(iris, 1e6, TRUE)
+#' microbenchmark::microbenchmark(
+#'   tapply = prop_tapply(x, "Sepal.Length"),
+#'   proportion = proportion(x, "Sepal.Length"),
+#'   times = 10
+#' )
+#' microbenchmark::microbenchmark(
+#'   tapply = prop_tapply(x$Species),
+#'   proportion = proportion(x$Species),
+#'   times = 100
+#' )
+#'
+#' ## more groups (very slow) prop_tapply excels
+#' x <- runif(1e5)
+#' microbenchmark::microbenchmark(
+#'   tapply = prop_tapply(x),
+#'   proportion = proportion(x),
+#'   times = 2
+#' )
+#' }
 #' @export
 
-proportion <- function(x, .name) {
-  UseMethod("proportion")
+proportion <- function(x, ...) {
+  UseMethod("proportion", x)
 }
 
 #' @export
-proportion.default <- function(x, .name = NULL) {
-  groups <- sort(unique(x))
-  res <- vapply(groups, function(.x) mean(.x == x), double(1), USE.NAMES = T)
-  names(res) <- groups
+proportion.default <- function(x, ...) {
+  gr <- unique(x)
+  res <- vapply(gr, function(.x) mean(.x == x), double(1))
+  names(res) <- gr
   res
 }
 
 #' @export
-proportion.tbl <- function(x, .name) {
-  col <- as.character(substitute(.name))
+proportion.data.frame <- function(x, col, ...) {
+  vals <- x[[col]]
+  col_name <- col
 
-  groups <- sort(unique(x[[col]]))
-  props <- vapply(groups, function(x) mean(x[[col]]), double(1), USE.NAMES = T)
-
-  tibble::enframe(props, col, "prop")
-}
-
-#' @export
-proportion.data.frame <- function(x, .name) {
-
-  if(is.name(substitute(.name))) {
-    vals <- eval(substitute(.name), x, enclos = parent.frame())
-    col_name <- deparse(substitute(.name))
-  } else {
-    vals <- x[[.name]]
-    col_name <- .name
-  }
-
-  groups <- sort(unique(vals))
+  groups <- unique(vals)
 
   res <- data.frame(
     col_name = groups,
-    props = vapply(groups, function(.x) mean(.x == vals), double(1))
+    props = vapply(groups, function(.x) mean(.x == vals), double(1)),
+    stringsAsFactors = FALSE,
+    fix.empty.names = FALSE
   )
 
   names(res)[1] <- col_name
   res
 }
 
+#' @export
+#' @rdname proportion
+prop_tapply <- function(x, ...) {
+  UseMethod("prop_tapply", x)
+}
 
+#' @export
+#' @rdname proportion
+prop_tapply.default <- function(x, ...) {
+  tapply(x, x, length) / length(x)
+}
+
+#' @export
+#' @rdname proportion
+prop_tapply.data.frame <- function(x, col, ...){
+  tapply(x[,col], x[,col], length) / nrow(x)
+}
+
+#' @export
+#' @rdname proportion
+cum_prop <- function(x) {
+  x <- cumsum(x)
+  x / x[length(x)]
+}
