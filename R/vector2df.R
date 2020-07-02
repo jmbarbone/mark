59e822fc8c010980093d7e4a1201a46a316c1481
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-07-02 15:43:18 -0400

    cleans up vector2df

--- R/vector2df.R
+++ R/vector2df.R
@@ -2,22 +2,26 @@
 #'
 #' Transforms a vector (named) to a data.frame
 #'
-#' @param .vector A vector of values.
-#' @param show_NA Logical.
+#' @param vec A vector of values.
+#' @param name,value Character strings for the name and value columns
+#' @param show_NA Logical
 #' @export
 
-vector2df <- function(.vector, show_NA = F)
-{
-  if(!is.vector(.vector)) stop("Object is not a vector.", call. = FALSE)
-  vector_names <- names(.vector)
-  if(show_NA) {
-    vector_names[vector_names == ""] <- NA
-  } else if(is.null(vector_names) || any(vector_names == "")) {
+vector2df <- function(vec, name = "name", value = "value", show_NA = FALSE) {
+  if (!is.vector(vec)) stop("Object is not a vector.", call. = FALSE)
+  vector_names <- names(vec)
+
+  if (show_NA) {
+    vector_names[vector_names == ""] <- NA_character_
+  } else if (is.null(vector_names) || any(vector_names == "")) {
     warning("Name missing from vector.", call. = FALSE)
   }
-  data.frame(
-    names = vector_names,
-    values = unname(.vector),
-    stringsAsFactors = FALSE
-  )
+
+  out <- data.frame(name = vector_names,
+                    value = unname(vec),
+                    stringsAsFactors = FALSE,
+                    fix.empty.names = FALSE,
+                    check.names = FALSE)
+  colnames(out) <- c(name, value)
+  out
 }
