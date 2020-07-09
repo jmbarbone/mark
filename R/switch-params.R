78622c114317f6ba7cab5443c433920467601db5
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-07-09 09:20:37 -0400

    adds functionf or dynamic params in `switch()`

---
+++ R/switch-params.R
@@ -0,0 +1,16 @@
+#' Switch with a list of parameters
+#'
+#' Switch with a list of params
+#'
+#' @references
+#' Original: https://stackoverflow.com/a/32835930/12126576
+#'
+#' @param x A single value
+#' @param params A list of cases
+#'
+#' @export
+
+switch_params <- function(x, params) {
+  stopifnot(is.list(params), length(x) == 1L)
+  do.call(switch, c(x, params))
+}
