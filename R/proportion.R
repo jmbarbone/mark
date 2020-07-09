8d313acc9a9ce4c20738fe678072cbc9b3c59855
Jordan Mark Barbone <jmbarbone@gmail.com>
2020-07-09 08:22:03 -0400

    removes tapply note; adds step to skip applying unnecessary as.factor

--- R/proportion.R
+++ R/proportion.R
@@ -2,10 +2,6 @@
 #'
 #' Calculates a proportion from a vector or data.frame/matrix.
 #'
-#' @details
-#' tapply(x, x, length) / length(x) can be speedier for smaller data sets and groups
-#'   but slows down with more data or a greater number of groups.
-#'
 #' @param x A data.frame or a vector.
 #' @param ... Additional arguments to be passed to methods.
 #' @param col A character string of the column name which holds the groups
@@ -23,7 +19,10 @@ proportion <- function(x, ...) {
 #' @export
 #' @rdname proportion
 proportion.default <- function(x, ...) {
-  vapply(split(x, as_factor_unordered(x)), length, double(1)) / length(x)
+  if (!inherits(x, "factor")) {
+    x <- as_factor_unordered(x)
+  }
+  vapply(split(x, x), length, double(1)) / length(x)
 }
 
 #' @export
