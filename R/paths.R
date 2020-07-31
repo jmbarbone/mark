32b4da59930bd72b5cdab9d858019ae4e2201e89
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-07-31 10:00:49 -0400

    adds `file_path()`; adds default to `path_norm()`

--- R/paths.R
+++ R/paths.R
@@ -6,11 +6,12 @@
 #' @param check Logical, if TRUE will check if the path exists and output a
 #'   warning if it does not.
 #' @param remove Logical, if TRUE will remove paths that are not found
+#' @param ... Character vectors for creating a path
 #'
 #' @export
 
-path_norm <- function(x, check = FALSE, remove = check) {
-  paths <- normalizePath(x, .Platform$file.sep, mustWork = FALSE)
+path_norm <- function(x = ".", check = FALSE, remove = check) {
+  paths <- normalizePath(x, winslash = .Platform$file.sep, mustWork = FALSE)
   ind <- !file.exists(paths)
 
   if (check && any(ind)) {
@@ -24,4 +25,11 @@ path_norm <- function(x, check = FALSE, remove = check) {
   }
 
   paths
+}
+
+#' @export
+#' @rdname path_norm
+file_path <- function(..., check = FALSE, remove = check) {
+  fp <- file.path(..., fsep = .Platform$file.sep)
+  path_norm(fp, check = check, remove = remove)
 }
