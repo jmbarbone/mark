bc1273a9ffcfcd19b26eeb3eeaac67e786493b0b
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-08-28 12:37:26 -0400

    include utils::globalVariables

--- R/jordan.R
+++ R/jordan.R
@@ -3,5 +3,6 @@
 #' Just Other R Data Analytic Nuggets.  This package contains a variety of functions and wrappers developed for personally and professional use.
 #'
 #' @docType package
+#' @importFrom utils globalVariables
 #' @name jordan
 "_PACKAGE"
