1c2394444e93a9288b88df04655c29f2b8fc460d
Jordan Mark Barbone <jmbarbone@gmail.com>
2020-11-29 13:17:21 -0500

    add use_author()

---
+++ R/author.R
@@ -0,0 +1,82 @@
+#' Add author to DESCRIPTION
+#'
+#' Adds author to description
+#'
+#' @details
+#' Only valid for a single author.
+#'
+#' @param author_info Author information as a named list
+#' @export
+
+use_author <- function(author_info = find_author()) {
+  stopifnot(
+    "author_info must be a list" = is.list(author_info),
+    "author_info should not be a person object" = !inherits(author_info, "person")
+  )
+
+  lines <- readLines("DESCRIPTION")
+  start <- grep("^[Aa]uthor", lines)
+
+  if (!length(start)) {
+    stop("Jordan needs to review this, sorry")
+    start <- grep("^[Vv]ersion", lines)
+    lines <- c(lines[1:start], "Author: ", lines[(start + 1):length(lines)])
+  }
+
+  spaces <- grepl("^\\s", lines)
+  end <- which.max(!spaces[-c(1:start)]) + start - 1
+
+  names(author_info) <- tolower(names(author_info))
+  nm <- names(author_info)
+  valid_names <- c("given", "family", "middle", "email", "role", "comment")
+  ok <- nm %in% valid_names & sapply(author_info, check_field)
+  author_info <- author_info[ok]
+
+  body <- author_info_to_text(author_info)
+  n <- length(body)
+  new_body <- c("Authors@R:",
+                paste0("    person(", trimws(body[1], "left")),
+                if (n > 2) paste0("           ", body[2:(n - 1)]) else NULL,
+                paste0("           ", sub("[,]$", "", body[n]), ")"))
+
+  out <- c(lines[1:(start - 1)],
+           new_body,
+           lines[(end + 1):length(lines)])
+
+  writeLines(out, "DESCRIPTION")
+}
+
+check_field <- function(x) {
+  is.character(x) && x != ""
+}
+
+author_info_to_text <- function(x) {
+  nm <- names(x)
+  width <- max(sapply(nm, nchar))
+
+  comment <- nm == "comment"
+  len <- sapply(x, length) == 1
+
+  ind <- !comment & len
+  x[ind] <- paste0('"', x[ind], '"')
+  # ind <- !comment & !len
+  # x[ind] <- paste0("c(", paste0('"', x[ind], '"', collapse = ","), ")")
+  # I don't think this will work with multiple items...
+  x[comment] <-  paste0("c(", names(x[comment][[1]]), " = ",  paste0('"', x[comment][1], '"'), ")")
+
+  paste0(format(nm, width = width), " = ", x, ",")
+}
+
+find_author <- function() {
+  author <- getOption("jordan.author")
+  if (!is.null(author)) {
+    return(author)
+  }
+
+  stop(
+    "Author information not found in options.\n",
+    "You can set the author information with options(jordan.author = ...)\n",
+    "  probably within an .Rprofile",
+    call. = FALSE
+  )
+}
