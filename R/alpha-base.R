05fc0f0dfc0b8c92b95563093facce063aa3b920
Jordan Mark Barbone <jmbarbone@gmail.com>
2020-12-01 16:56:44 -0500

    add base_n(); renaming alpha base formatting; testins for base_*()

--- R/alpha-base.R
+++ R/alpha-base.R
@@ -3,44 +3,88 @@
 #' Base 26 conversion with letters
 #'
 #' @param x A string of letters.  Non characters are removed.
-#'
-#' @importFrom stats na.omit
+#' @param base A numeric
 #'
 #' @export
 #'
 #' @examples
-#' alpha_base("AB")
-#' alpha_base("XFD")
-#' alpha_base(c("JMB", "Jordan Mark", "XKCD"))
+#' base_alpha("AB")
+#' base_alpha("XFD")
+#' base_alpha(c("JMB", "Jordan Mark", "XKCD"))
+#' sum(base_alpha(c("x", "k", "c", "d")))
 
-alpha_base <- function(x)  {
-  vapply(x, function(x) {
-    lets <- unlist(strsplit(tolower(x), ""), use.names = FALSE)
-    lets <- lets[grepl("[[:alpha:]]", lets)]
-    valid <- match(lets, letters, nomatch = NULL)
-    if(anyNA(valid)) {
-      stop("invalid character(s) found: ", paste(lets[is.na(valid)], collapse = ", "))
-    }
-    vals <- na.omit(valid)
-    n <- length(vals)
-    sum(c(vals[-n] * 26 ^ rev(seq(n-1)), vals[n]))
-  },
-  numeric(1), USE.NAMES = FALSE)
+base_alpha <- function(x, base = 26)  {
+  stopifnot(is.character(x))
+  check_base_alpha(base, high = 26)
+  vap_int(x, alpha_base_single, base = base)
 }
 
-# base_n <- function(x, base) {
-#   mapply(function(x, base) {
-#     nums <- unlist(strsplit(x, ""), use.names = FALSE)
-#     nums <- as.numeric(nums)
-#     valid <- !is.na(nums)
-#     if(anyNA(valid)) {
-#       stop("invalid character(s) found: ", paste(nums[is.na(valid)], collapse = ", "))
-#     }
-#     vals <- na.omit(nums)
-#     n <- length(vals)
-#     sum(c(vals[-n] * base ^ rev(seq(n - 1)), vals[n]))
-#   }, x, base, USE.NAMES = FALSE)
-# }
-#
-# base_n("18", 5)
-# base_n("25", 1:10)
+alpha_base <- function(x, base = 26) {
+  warning("Use jordan::base_alpha() instead", call. = FALSE)
+  base_alpha(x, base = base)
+}
+
+base_alpha_single <- function(x, base) {
+  a <- letters %wi% chr_split(tolower(x))
+  a <- match(a, letters[1:base], nomatch = NA_integer_)
+  if (anyNA(a)) {
+    stop(sprintf('Cannot calculate alpha base "%s" for "%s" which has letters beyond "%s"',
+                 base, x, x[base]),
+         call. = FALSE)
+  }
+  n <- length(a)
+  if (n == 0) {
+    return(NA_integer_)
+  }
+  as.integer(sum(c(a[-n] * base^(1:(n - 1)), a[n])))
+}
+
+
+#' Base N conversion
+#'
+#' Convert between base numbers (1 through 10)
+#'
+#' @param x A vector of integers
+#' @param base An integer base (valid 1 through 10)
+#'
+#' @export
+base_n <- function(x, base = 10) {
+  stopifnot(is.numeric(x))
+
+  if (base == 10) {
+    return(x)
+  }
+
+  check_base(base)
+  vap_int(x, base_n_single, base = base)
+}
+
+base_n_single <- function(x, base) {
+  ints <- as.integer(chr_split(x))
+
+  if (any(ints >= base)) {
+    stop("Cannot caluclate base \'", base, "\' for \'", x, "\' which has ",
+         "numbers greater than or equal to the base value", call. = FALSE )
+  }
+
+  seqs <- (length(ints) - 1L):0L
+  as.integer(sum(mapply(function(i, s) i * base^s, i = ints, s = seqs)))
+}
+
+check_base_alpha <- function(b, high = 26) {
+  if (is.character(b)) {
+    b <- chr_split(b)
+    stopifnot(length(b) == 1)
+    b <- match(tolower(b), letters)
+  }
+
+  check_base(b, high = high)
+}
+
+check_base <- function(b, high = 9) {
+  stopifnot("base must be an integer" = b %% 1 == 0)
+
+  if (b > high | b <= 1) {
+    stop("base must be between 1 and ", high, call. = FALSE)
+  }
+}
---
+++ tests/testthat/test-base-conversion.R
@@ -0,0 +1,17 @@
+test_that("base_alpha() works as expected", {
+  expect_equal(sum(base_alpha(c("x", "k", "c", "d"))), 42)
+
+  expect_equal(base_alpha(letters),
+               base_alpha(LETTERS))
+
+  letter_vec <- paste_c(c("", letters), c(letters), TRUE)
+  expect_equal(base_alpha(letter_vec), 1:702)
+
+  expect_error(base_alpha(letters[1:10], 9))
+  expect_warning(alpha_base(letters))
+})
+
+test_that("base_n() works as expected", {
+  expect_equal(base_n(120, 9), 99)
+  expect_error(base_n(62, 2))
+})
