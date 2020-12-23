d8d21591f6bb1c714a10d1f9297fb2e2cc0506ad
Jordan Mark Barbone <jmbarbone@gmail.com>
2020-12-22 19:05:29 -0500

    update paste_combine; add collapse()

--- R/paste.R
+++ R/paste.R
@@ -2,23 +2,65 @@
 #'
 #' Paste and combine
 #'
-#' @param x,y Vectors to paste and combine
-#' @param collate Logical; `TRUE` prints out combinations in order of vector
-#'   `x` elements then `y`; otherwise reversed (see examples)
+#' @param x,y,... Vectors to paste and/or combine
+#' @param collate Logical; `TRUE` prints out combinations in order of the first
+#'   vector elements then the next; otherwise reversed (see examples)
 #' @param sep A character string to separate terms
 #' @export
+#'
+#' @name utils-paste
 #'
 #' @examples
 #' x <- letters[1:5]
 #' y <- 1:3
-#' paste_c(x, y)
-#' paste_c(x, y, sep = "_")
-#' paste_c(x, y, collate = FALSE)
+#' z <- month.abb[c(1, 12)]
+#' paste_combine(x, y)
+#' paste_combine(x, y, z)
+#' paste_combine(x, y, sep = "_")
+#' paste_combine(x, y, collate = FALSE)
 
 paste_c <- function(x, y, collate = TRUE, sep = "") {
+  warning("Use `jordan::paste_combine()` instead", call. = FALSE)
+  paste_combine(x, y, collate = collate, sep = sep)
+}
+
+
+#' @rdname utils-paste
+#' @export
+paste_combine <- function(..., collate = TRUE, sep = "") {
+  ls <- list(...)
+  n <- length(ls)
+  stopifnot("length of ... must be at least 2" = n >= 2)
+
+  out <- do_paste_combine(ls[[1]], ls[[2]], collate = collate, sep = "")
+
+  if (n == 2) {
+    return(out)
+  }
+
+  for (i in 1:(-2)) {
+    out <- do_paste_combine(out, ls[[i]], collate = collate, sep = sep)
+  }
+
+  out
+}
+
+do_paste_combine <- function(x, y, collate = TRUE, sep = "") {
   out <- outer(x, y, FUN = paste, sep = sep)
   if (collate) {
     out <- apply(out, 1, c)
   }
   as.vector(out, "character")
+}
+
+#' @rdname utils-paste
+#' @export
+collapse <- function(..., sep = " ") {
+  paste(..., sep = NULL, collapse = sep)
+}
+
+#' @rdname utils-paste
+#' @export
+collapse0 <- function(...) {
+  collapse(..., sep = "")
 }
