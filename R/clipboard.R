68aa51ec05bf59b6cd150e8355ccd0b4363b1aba
Jordan Mark Barbone <jmbarbone@gmail.com>
2021-01-04 08:24:02 -0500

    try to determine vector classes
    * read_cllipboard() for vectors tries to determine the class
    * read_clipboard() for data.fames ties to determine classes for each column
    * formatting

--- R/clipboard.R
+++ R/clipboard.R
@@ -17,7 +17,7 @@ write_clipboard <- function(x = .Last.value, ...) {
 
 #' @export
 write_clipboard.default <- function(x = .Last.value, format = 1L, ...) {
-  utils::writeClipboard(str = x, format = format, ...)
+  utils::writeClipboard(str = as.character(x), format = format, ...)
 }
 
 #' @export
@@ -42,26 +42,34 @@ read_clipboard <- function(method = c("default", "data.frame", "tibble"), ...) {
   switch(
     match_param(method),
 
-    default = utils::readClipboard(),
+    default = try_vector_formats(utils::readClipboard()),
 
     # Specifications I prefer -- mostly copying from Excel
-    data.frame = utils::read.table(
-      file = "clipboard-128",
-      header = TRUE,
-      # Copying form Excel produces tab separations
-      sep = "\t",
-      row.names = NULL,
-      # Excel formula for NA produces #N/A -- sometimes people use N/A...
-      na.strings = c("", "NA", "N/A", "#N/A"),
-      check.names = FALSE,
-      stringsAsFactors = FALSE,
-      encoding = "UTF-8",
-      # occasionally "#' is used as a column name -- may cause issues
-      comment.char = "",
-      blank.lines.skip = FALSE,
-      fill = TRUE,
-       ...
-    ),
+    data.frame = {
+      tab <- utils::read.table(
+        file = "clipboard-128",
+        header = TRUE,
+        # Copying form Excel produces tab separations
+        sep = "\t",
+        row.names = NULL,
+        # Excel formula for NA produces #N/A -- sometimes people use N/A...
+        na.strings = c("", "NA", "N/A", "#N/A"),
+        check.names = FALSE,
+        stringsAsFactors = FALSE,
+        encoding = "UTF-8",
+        # occasionally "#' is used as a column name -- may cause issues
+        comment.char = "",
+        blank.lines.skip = FALSE,
+        fill = TRUE,
+        ...
+      )
+
+      for (i in seq_along(tab)) {
+        tab[[i]] <- try_vector_formats(tab[[i]])
+      }
+
+      tab
+      },
 
     tibble = tibble::as_tibble(read_clipboard("data.frame", ...))
     )
@@ -70,3 +78,36 @@ read_clipboard <- function(method = c("default", "data.frame", "tibble"), ...) {
 clear_clipboard <- function() {
   utils::writeClipboard("")
 }
+
+try_vector_formats <- function(x) {
+  if (!is.character(x)) {
+    return(x)
+  }
+
+  # Take subset to determine
+  x0 <- trimws(x[1:min(length(x), 1000)])
+
+  if (all(x0 %in% c("TRUE", "FALSE", "NA"))) {
+    return(as.logical(x))
+  }
+
+  if (all(grepl("^-?[[:digit:]]+$", x0))) {
+    return(as.integer(x))
+  }
+
+  if (all(grepl("^-?[[:digit:]]+\\.?([[:digit:]]+)?$", x0))) {
+    return(as.double(x))
+  }
+
+  if (all(grepl("^-?\\.[[:digit:]]+$", x0))) {
+    return(as.double(x))
+  }
+
+  dates <- as_date_strptime(x0)
+  if (isTRUE(all.equal(as.character(dates), x0))) {
+    return(dates)
+  }
+
+  x
+}
+
