208d764c848e5f76eb716b6b8709ede437cdd5c5
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2021-01-05 22:02:37 -0500

    correct soucing params

--- R/sourcing.R
+++ R/sourcing.R
@@ -3,40 +3,43 @@
 #' Functions for extending sourcing features
 #'
 #' @param file An R or Rmd file.
-#' @param ... Additional arguments, see details.
 #' @param quiet Logical; Determines whether to apply silence to [knitr::purl()]
 #' @param cd Logical; if TRUE, the R working directory is temporarily
 #'   changed to the directory containing file for evaluating
+#' @param env An environment determining where the parsed expressions are
+#'   evaluated
+#' @param ... Additional arguments passed to [base::source()]
 #'
 #' @details
 #'
-#' `try_source()` will output an error message rather than completely preventing the execution.
-#' This can be useful for when a script calls on multiple, independent files to be sourced
-#' and a single failure shouldn't prevent the entire run to fail as well.
-#'
-#' `ksource()` ... sends additional arguments to [knitr::purl()]
+#' `try_source()` will output an error message rather than completely preventing
+#'   the execution.
+#' This can be useful for when a script calls on multiple, independent files to
+#'   be sourced and a single failure shouldn't prevent the entire run to fail as
+#'   well.
 #'
 #' @name sourcing
 #' @export
 
-ksource <- function(file, ..., quiet = TRUE, cd = FALSE)
-{
+ksource <- function(file, ..., quiet = TRUE, cd = FALSE, env = parent.frame()) {
+  stopifnot(is.environment(env))
+
   require_namespace("knitr")
   source(
     knitr::purl(
       file,
       output = tempfile(),
       quiet = quiet,
-      ...
     ),
-    chdir = cd
+    chdir = cd,
+    local = env
   )
 }
 
 
 #' @rdname sourcing
 #' @export
-try_source <- function(file, ..., cd = FALSE) {
+try_source <- function(file, cd = FALSE, ...) {
   tryCatch(
     source(file, chdir = cd),
     error = function(e) {
@@ -49,9 +52,9 @@ try_source <- function(file, ..., cd = FALSE) {
 
 #' @rdname sourcing
 #' @export
-try_ksource <- function(file, ..., cd = FALSE) {
+try_ksource <- function(file, ...) {
   tryCatch(
-    ksource(file = file, ..., cd = cd),
+    ksource(file = file, ...),
     error = function(e) {
       warning(e, call. = FALSE)
     },
@@ -161,6 +164,7 @@ source_r_file <- function(path, echo = FALSE, quiet = FALSE, ...) {
                     basename(path),
                     round(st[["elapsed"]], 2)))
   }
+
   invisible()
 }
 
