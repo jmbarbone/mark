0afb274b870c398055782a4612c238f2c85d19c1
jmbarbone <jmbarbone@gmail.com>
2021-02-09 22:45:49 -0500

    remove unused functions

--- R/utils.R
+++ R/utils.R
@@ -58,24 +58,6 @@ ept <- function(x, envir = parent.frame()) {
   eval(parse(text = x), envir = envir)
 }
 
-construct_date <- function(date_text, sep = "") {
-  collapse0(vap_chr(unlist(strsplit(date_text, "")), date_switch), sep = sep)
-}
-
-date_switch <- function(x) {
-  switch(x,
-         Y = "[[:digit:]]{4}",
-         y = "[0-99]{2}",
-         m = "1[0-2]|[1-9]",
-         b = "[[:alpha:]]{3,}",
-         d = "3[01]|[12][0-9]|[1-9]",
-         NULL)
-}
-
-# construct_date("ymd")
-# construct_date("bdY")
-# grepl(construct_date("bdY", "[\\s|,]+"), "December 17, 1992", ignore.case = TRUE)
-# grepl(construct_date("Ymd", "-"), "1992-12-17", ignore.case = TRUE)
 
 # Removes object's attributes before printing
 print_no_attr <- function(x, ...) {
