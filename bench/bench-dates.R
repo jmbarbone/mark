aea3c96c0df2339bdd52e7cabcf73d638d0555ac
jmbarbone <jmbarbone@gmail.com>
2021-02-09 22:46:33 -0500

    Update bench-dates.R

--- bench/bench-dates.R
+++ bench/bench-dates.R
@@ -5,6 +5,7 @@ n <- 1e4
 
 year <- sample(0:3000, n, TRUE)
 month <- sample(c("UN", "XX", month.abb), n, TRUE)
+month[sample(1e4, 1e2)] <- sample(0:12, 1e2, TRUE)
 day <- sample(0:32, n, TRUE)
 
 ymd <- paste( year, month,  day, sep = "-")
@@ -16,5 +17,9 @@ bench::mark(
   date_from_partial(ymd),
   date_from_partial(dmy, "dmy"),
   date_from_partial(mdy, "mdy"),
-  iterations = 20
+  iterations = 5,
+  check = FALSE
 )
+
+# bench:::autoplot.bench_mark(.Last.value)
+
