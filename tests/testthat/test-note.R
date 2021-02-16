a84c9c522fc0f2cb5807a762e157bd164d0b1149
jmbarbone <jmbarbone@gmail.com>
2021-02-16 13:03:18 -0500

    simplify note tests
    No longer worrying about custom printing functions

--- tests/testthat/test-note.R
+++ tests/testthat/test-note.R
@@ -1,42 +1,11 @@
 test_that("Assignments work", {
-  op <- options()
-
-  # To default
-  options(jordan.note.fun = NULL)
-
   x <- "x"
   nt <- "this is a note"
   note(x) <- nt
-  out <- suppressMessages(note(x))
+  out <- note(x)
 
   expect_visible(out)
-  expect_s3_class(out, "jordan_note")
+  expect_s3_class(x, "noted")
+  expect_s3_class(out, "note")
   expect_equal(as.character(out), nt)
-
-  on.exit(do.call(options, as.list(op)))
-})
-
-test_that("Options", {
-  op <- options()
-  # setting to default
-  options(jordan.note.fun = NULL)
-
-  x <- "x"
-  note(x) <- "note"
-  expect_invisible(suppressMessages(note(x)))
-  expect_message(note(x))
-
-  options(jordan.note.fun = message)
-  expect_message(note(x))
-
-  options(jordan.note.fun = warning)
-  expect_warning(note(x))
-
-  options(jordan.note.fun = stop)
-  expect_error(note(x))
-
-  options(jordan.note.fun = "not a function")
-  expect_error(note(x))
-
-  on.exit(do.call(options, as.list(op)))
 })
