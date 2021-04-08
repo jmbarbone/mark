b990dd60194d10e55a5ba9b5341ae6635f36885a
Jordan Mark Barbone <jmbarbone@gmail.com>
2021-04-08 08:18:08 -0400

    organize files

---
+++ R/limit.R
@@ -0,0 +1,20 @@
+#' Limit
+#'
+#' Limit a numeric vector by lower and upper bounds
+#'
+#' @param x A numeric vector
+#' @param lower A lower limit (as `x < lower`)
+#' @param upper An upper limit (as `x > higher`)
+#'
+#' @export
+limit <- function(x, lower = min(x), upper = max(x)) {
+  stopifnot(
+    "lower must be a single value" = is.numeric(lower) && length(lower) == 1L,
+    "lower must be a single value" = is.numeric(upper) && length(upper) == 1L,
+    "lower cannot be more than upper" = lower > upper
+  )
+
+  x[x < lower] <- lower
+  x[x > upper] <- upper
+  x
+}
