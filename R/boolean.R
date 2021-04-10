6b9790b2571eb02788638ef563fedf2ee302132d
jmbarbone <jmbarbone@gmail.com>
2021-04-09 23:15:47 -0400

    adds temporary to_boolean()

---
+++ R/boolean.R
@@ -0,0 +1,47 @@
+#' To Boolean
+#'
+#' Convert a vector to boolean/logical
+#'
+#' @param x A vector of values
+#' @param true A vector of values to convert to `TRUE`
+#' @param false A vector of values to convert to `FALSE`
+to_boolean <- function(x, ...) {
+  UseMethod("to_boolean", x)
+}
+
+#' @rdname to_boolean
+to_boolean.logical <- function(x, ...) {
+  x
+}
+
+#' @rdname to_boolean
+to_boolean.numeric <- function(x, true = 1L, false = 0L) {
+  out <- rep(NA, length(x))
+  out[x %in% true] <- TRUE
+  out[x %in% false] <- FALSE
+  out
+}
+
+#' @rdname to_boolean
+to_boolean.character <- function(x, true = NULL, false = NULL) {
+  if (is.null(trues) && is.null(falses)) {
+    x <- fact(x)
+    return(to_boolean_default(x))
+  }
+}
+
+#' @rdname to_boolean
+to_boolean.factor <- function(x, true, false) {
+  if (is.null(trues) && is.null(falses)) {
+    return(to_boolean_default(x))
+  }
+}
+
+to_boolean_default <- function(x) {
+  u <- levels(x)
+  lu <- trimws(tolower(u))
+  res <- rep_len(NA, length(u))
+  res[lu %in% c("true", "t", "yes", "y")] <- TRUE
+  res[lu %in% c("false", "f", "no", "n")] <- FALSE
+  res[match(x, u)]
+}
