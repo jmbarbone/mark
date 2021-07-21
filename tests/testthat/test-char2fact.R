09cf9a621d6381a188b669258b6a11ed8dbfa19c
jmbarbone <jmbarbone@gmail.com>
2021-07-20 23:47:55 -0400

    adds tests for char2fact

---
+++ tests/testthat/test-char2fact.R
@@ -0,0 +1,23 @@
+test_that("char2fact() works", {
+  expect_error(char2fact(1L))
+
+  x <- letters[c(1, 2, 2, 2, 3, 4, 4, 5, 5)]
+  expect_equal(char2fact(x), fact(x))
+
+  # Threshold
+  y <- c(x, "j")
+  expect_equal(y, char2fact(y, 5))
+  expect_equal(fact(y), char2fact(y, 6))
+})
+
+test_that("char2fact.data.frame() works", {
+  x <- data.frame(
+    a = letters[c(1, 2, 2, 2, 3, 4, 4, 5, 5)],
+    b = letters[c(1, 2, 3, 4, 5, 6, 7, 8, 9)]
+  )
+
+  y <- x
+  y[["a"]] <- fact(y[["a"]])
+
+  expect_equal(char2fact(x), y)
+})
