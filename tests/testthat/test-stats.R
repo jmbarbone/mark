4fe9739b0aa72579303c912156f7428469e3c61b
jmbarbone <jmbarbone@gmail.com>
2021-07-21 23:24:22 -0400

    Create test-stats.R

---
+++ tests/testthat/test-stats.R
@@ -0,0 +1,13 @@
+test_that("median2() works", {
+  x <- runif(100)
+  expect_equal(median2(x), median(x))
+  expect_equal(
+    median2(x, type = 3),
+    quantile(x, type = 3, probs = .5, names = FALSE)
+  )
+})
+
+test_that("range2() works", {
+  x <- runif(100)
+  expect_equal(range(x), c(min(x), max(x)))
+})
