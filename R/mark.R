993937600f4f60936bac5fc59971c95e64775a2a
jmbarbone <jmbarbone@gmail.com>
2021-08-01 19:22:11 -0400

    adds list of options

--- R/mark.R
+++ R/mark.R
@@ -5,3 +5,16 @@
 #' @docType package
 #' @name mark
 "_PACKAGE"
+
+
+op.mark <- list(
+  mark.author = NULL,
+  # control for check_interactive() to return interactive() or TRUE
+  mark.check_interactive = TRUE,
+  mark.note.width = NULL,
+
+  mark.days_in_month = 30,
+  mark.days_in_year = 365,
+  mark.weeks_inn_year = 52,
+  mark.default_tz = "UTC"
+)
