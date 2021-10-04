1a4cb30d1e4f6e2f3d7d2ed91ff9912afab69d47
jmbarbone <jmbarbone@gmail.com>
2021-10-03 23:03:07 -0400

    adds more tests

--- tests/testthat/test-boolean.R
+++ tests/testthat/test-boolean.R
@@ -12,6 +12,7 @@ test_that("to_boolean.numeric() works", {
 
 wtf <- c("what", "true", "T", "TRUE", "false", "F", "false")
 yn <- c("Y", "yes", "N", "no")
+
 test_that("to_boolean.character() works", {
   expect_identical(
     to_boolean(wtf),
@@ -36,3 +37,50 @@ test_that("to_boolean.factor() works", {
   )
 })
 
+test_that("is_true(), is_false() works", {
+  x <- c(2, 3, NA)
+  exp <- c(NA, NA, NA)
+
+  res <- is_true(x)
+  expect_identical(exp, res)
+
+  res <- is_false(x)
+  expect_identical(res, res)
+
+  x <- c(TRUE, FALSE, NA)
+
+  res <- is_true(x)
+  exp <- c(TRUE, FALSE, FALSE)
+  expect_identical(res, exp)
+
+  res <- is_false(x)
+  exp <- c(FALSE, TRUE, FALSE)
+  expect_identical(res, exp)
+})
+
+test_that("%xor% works", {
+  x <- TRUE
+  y <- FALSE
+
+  expect_identical(xor(x, y), x %xor% y)
+})
+
+test_that("either() works", {
+  x <- c(TRUE, NA, FALSE, FALSE)
+  y <- c(TRUE, TRUE, FALSE, NA)
+
+  res <- either(x, y)
+  exp <- c(TRUE, TRUE, FALSE, FALSE)
+  expect_identical(res, exp)
+})
+
+
+test_that("none() works", {
+  x <- c(FALSE, FALSE, NA)
+  expect_identical(none(x), NA)
+  expect_true(none(x, na.rm = TRUE))
+
+  x <- c(TRUE, NA)
+  expect_false(none(x))
+  expect_false(none(x, na.rm = TRUE))
+})
