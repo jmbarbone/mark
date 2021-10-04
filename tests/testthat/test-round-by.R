5e3ddaa0090fdf67f02a8200e669aa073c8917ee
jmbarbone <jmbarbone@gmail.com>
2021-10-03 21:57:54 -0400

    adds include0 test

--- tests/testthat/test-round-by.R
+++ tests/testthat/test-round-by.R
@@ -1,11 +1,15 @@
 test_that("round_by() works", {
   x <- seq(1, 13, by = 4/3)
 
-  exp <- round_by(x, 1)
-  res <- c(1, 2, 4, 5, 6, 8, 9, 10, 12, 13)
-  expect_equal(exp, res)
+  res <- round_by(x, 1)
+  exp <- c(1, 2, 4, 5, 6, 8, 9, 10, 12, 13)
+  expect_equal(res, exp)
 
-  exp <- round_by(x, 2)
-  res <- c(0, 2, 4, 4, 6, 8, 8, 10, 12, 12)
-  expect_equal(exp, res)
+  res <- round_by(x, 2)
+  exp <- c(0, 2, 4, 4, 6, 8, 8, 10, 12, 12)
+  expect_equal(res, exp)
+
+  res <- round_by(x, 2, include0 = FALSE)
+  exp <- c(2, 2, 4, 4, 6, 8, 8, 10, 12, 12)
+  expect_equal(res, exp)
 })
