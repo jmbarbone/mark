25097cb88e761be4f95ec55937dce37801ef92c3
jmbarbone <jmbarbone@gmail.com>
2021-10-10 12:50:32 -0400

    Create test-namespace.R

---
+++ tests/testthat/test-namespace.R
@@ -0,0 +1,8 @@
+
+test_that("loadAllNamespace() works", {
+  expect_message(loadAllNamespace("magrittr", warn.conflicts = FALSE), NA)
+  detach("package:magrittr")
+  # should throw at least 1 message
+  muffle(expect_message(loadAllNamespace("magrittr")))
+  detach("package:magrittr")
+})
