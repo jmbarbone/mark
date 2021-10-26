fe668c56b6c3cf1fe3d65aeaa9e64c38b9a1d500
Jordan Mark Barbone <jbarbone@cogstate.com>
2021-10-26 10:23:50 -0400

    resolves #48

--- tests/testthat/test-detail.R
+++ tests/testthat/test-detail.R
@@ -9,3 +9,15 @@ test_that("details() works", {
 
   expect_error(detail(data.frame()))
 })
+
+test_that("details() and tibbles", {
+  skip_if_not_installed("tibble")
+
+  expect_error(detail(tibble::tibble(a = 1, b = 1:3)), NA)
+  expect_error(detail(tibble::tibble(a = 1, b = list(1:3))), NA)
+  expect_error(detail(tibble::tibble(a = NULL, b = list(1:3))))
+})
+
+test_that("details.data.frame() passes with single column [48]", {
+  expect_error(data.frame(a = 1), NA)
+})
