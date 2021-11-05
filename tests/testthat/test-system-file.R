6fc4ea1db713aefc078a80feb95404fa877c0032
jmbarbone <jmbarbone@gmail.com>
2021-11-04 22:06:42 -0400

    Update test-system-file.R

--- tests/testthat/test-system-file.R
+++ tests/testthat/test-system-file.R
@@ -1,3 +1,6 @@
 test_that("make_sf() ~works~ doesn't fail", {
-  expect_type(make_sf("foo"), "closure")
+  expect_type(sf0 <- make_sf("foo"), "closure")
+  expect_identical(sf0, sf, ignore_function_env = TRUE)
+  expect_identical(sf0(23232), "")
+  expect_error(sf0(23232, check = TRUE))
 })
