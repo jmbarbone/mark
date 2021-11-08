9c792b0e448dcf04460a4929a0b34dd71fca2e81
jmbarbone <jmbarbone@gmail.com>
2021-11-07 20:33:11 -0500

    adds tests for row_bind()

---
+++ tests/testthat/test-row-bind.R
@@ -0,0 +1,10 @@
+test_that("row_bind() works", {
+  res1 <- row_bind(quick_dfl(a = 1, b = 2), quick_dfl(b = 2, c = 3))
+  res2 <- row_bind(list(quick_dfl(a = 1, b = 2), quick_dfl(b = 2, c = 3)))
+  exp <- quick_dfl(a = c(1L, NA), b = c(2L, 2L), c = c(NA, 3L))
+  expect_identical(res1, exp)
+  expect_identical(res2, exp)
+
+  expect_error(row_bind(1))
+  expect_error(row_bind(list()))
+})
