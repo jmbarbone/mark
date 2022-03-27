f75aa35d859acd431116408da6dec25083bb7cfa
jmbarbone <jmbarbone@gmail.com>
2022-03-26 22:21:11 -0400

    clean up tests

--- tests/testthat/test-recode.R
+++ tests/testthat/test-recode.R
@@ -1,33 +1,31 @@
 test_that("recode_by() examples works", {
-  expect_equal(
-    recode_by(1:3, c(a = 1, b = 2)),
-    c("a", "b", NA_character_)
-  )
-  expect_equal(
-    recode_by(letters[1:3], c(`1` = "a", `2` = "b")),
-    c("1", "2", NA_character_)
-  )
+  res <- recode_by(1:3, c(a = 1, b = 2))
+  exp <- c("a", "b", NA_character_)
+  expect_identical(res, exp)
 
-  x <- recode_by(letters[1:3], c(`1` = "a", `2` = "b"), mode = "integer")
-  expect_equal(x, c(1, 2, NA_integer_))
-  expect_equal(x, recode_by(letters[1:3], c("a", "b"), vals = 1:2))
+  res <- recode_by(letters[1:3], c(`1` = "a", `2` = "b"))
+  exp <-  c("1", "2", NA_character_)
+  expect_identical(res, exp)
+
+  res <- recode_by(letters[1:3], c(`1` = "a", `2` = "b"), mode = "integer")
+  exp <- c(1L, 2L, NA_integer_)
+  expect_identical(res, exp)
+  exp <- recode_by(letters[1:3], c("a", "b"), vals = 1:2)
+  expect_identical(res, exp)
 })
 
 test_that("recode_only() examples work", {
-  expect_equal(
-    recode_only(letters[1:3], c("zzz" = "a")),
-    c("zzz", "b", "c")
-  )
+  res <- recode_only(letters[1:3], c("zzz" = "a"))
+  exp <- c("zzz", "b", "c")
+  expect_identical(res, exp)
 
-  expect_equal(
-    recode_only(letters[1:3], c(`1` = "a")),
-    c("1", "b", "c")
-  )
+  res <- recode_only(letters[1:3], c(`1` = "a"))
+  exp <- c("1", "b", "c")
+  expect_identical(res, exp)
 
-  expect_equal(
-    wuffle(recode_only(1:3, c("a" = 1))),
-    c(NA_integer_, 2, 3)
-  )
+  res <- wuffle(recode_only(1:3, c(a = 1L)))
+  exp <- c(NA_integer_, 2L, 3L)
+  expect_identical(res, exp)
 })
 
 test_that("clean_na_coercion() works", {
