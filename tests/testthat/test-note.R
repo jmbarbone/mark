e6b9a80ce49cc8b05ca9509f3c1e119b5d514560
jmbarbone <jmbarbone@gmail.com>
2022-07-23 01:04:01 -0400

    resolve #128

--- tests/testthat/test-note.R
+++ tests/testthat/test-note.R
@@ -1,4 +1,6 @@
 co_note <- function(x) {
+  op <- options(mark.check_interactive = FALSE)
+  on.exit(options(op), add = TRUE)
   out <- capture.output(print_note(x))
 
   if (use_color()) {
@@ -38,11 +40,13 @@ test_that("print.noted() passes to next methods [67]", {
 
   # not bothering with snapshots
   x <- tibble::tibble(a = 1:50)
+  original <- capture.output(print(x, n = 40))
   note(x) <- "note"
-  expect_match(
-    utils::tail(capture.output(print(x, n = 40)), 1),
-    "with 10 more rows"
-  )
+  co <- withr::with_options(list(mark.check_interactive = FALSE), {
+    capture.output(print(x, n = 40))
+  })
+  expect_true(all(original[-3] %in% co))
+  expect_identical(co[1], "Note : note")
 })
 
 test_that("print_note() works with data.frame", {
