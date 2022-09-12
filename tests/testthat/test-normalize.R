07d6d42ba7ee552bd5a9993fc1173301ef23c565
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-09-11 21:32:41 -0400

    add tests

---
+++ tests/testthat/test-normalize.R
@@ -0,0 +1,18 @@
+test_that("normalize.default() works", {
+  x <- 1:5
+  res <- normalize(x)
+  exp <- seq(0, 1, .25)
+  expect_identical(res, exp)
+
+  x <- matrix(1:5, ncol = 5)
+  res <- normalize(x)
+  exp <- matrix(seq(0, 1, .25), ncol = 5)
+  expect_identical(res, exp)
+})
+
+test_that("normalize.data.frame() works", {
+  x <- data.frame(a = 1:5, b = 6:10)
+  res <- normalize(x)
+  exp <- data.frame(a = seq(0, 1, .25), b = seq(0, 1, .25))
+  expect_identical(res, exp)
+})
