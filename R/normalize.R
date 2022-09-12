470ca45e9f4d728b120ed834da4fe2e0b8f495e2
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-09-11 22:07:08 -0400

    update docs and data.frame method

--- R/normalize.R
+++ R/normalize.R
@@ -2,20 +2,31 @@
 #'
 #' Normalizes values based on possible range and new bounds
 #'
-#' @param x An object that is (coercible to) `double`; `data.frames` are transformed
-#' @param range The range of possible values of `x` as a vector of length `2`.
-#'   Defaults to `range(x, na.rm = TRUE)`
-#' @param bounds The new boundaries for the normalized value as a vector of
-#'   length `2`.  Defaults to `c(0, 1)`.
+#' @details Parameters `range` and `bounds` are modified with [base::range()].
+#'   The largest and smallest values are then used to determine the
+#'   minimum/maximum values and lower/upper bounds.  This allows for a vector of
+#'   more than two values to be passed.
+#'
+#'   The current implementation of `normalize.data.frame()` allows for `list` of
+#'   parameters passed for each column.  However, it is probably best suited for
+#'   default values.
+#'
+#' @param x An object that is (coercible to) `double`; `data.frames` are
+#'   transformed
+#' @param range The range of possible values of `x`.  See details for more info.
+#'   Defaults to the range of non-`NA` values
+#' @param bounds The new boundaries for the normalized values of `x`. Defaults
+#'   to `0` and `1`.
+#' @param ... Additional arguments passed to methods
 #' @returns `x` with transformed values where `range` values are transformed to
 #'   `bounds`.
 #' @examples
 #' x <- c(0.23, 0.32, 0.12, 0.61, 0.26, 0.24, 0.23, 0.32, 0.29, 0.27)
 #' data.frame(
-#'   x = normalize(x),
-#'   v = normalize(x, range = c(0, 2)),
-#'   b = normalize(x, bounds = c(0, 10)),
-#'   vb = normalize(x, range = c(0, 2), bounds = c(0, 10))
+#'   x  = normalize(x),
+#'   v  = normalize(x, range = 0:2),
+#'   b  = normalize(x, bounds = 0:10),
+#'   vb = normalize(x, range = 0:2, bounds = 0:10)
 #' )
 #'
 #' # maintains matrix
@@ -24,14 +35,10 @@
 #' 0.34, 0.8, 0.25, 0.46, 0.03, 0.71, 0.79, 0.56, 0.83, 0.97), dim = c(10L, 3L))
 #'
 #' mat
-#' normalize(mat, bounds = c(-1, 1))
-#' normalize(as.data.frame(mat), bounds = c(-1, 1))
+#' normalize(mat, bounds = -1:1
+#' normalize(as.data.frame(mat), bounds = -1:1)
 #' @export
-normalize <- function(
-    x,
-    range = base::range(x, na.rm = TRUE),
-    bounds = 0:1
-) {
+normalize <- function(x, ...) {
   UseMethod("normalize")
 }
 
@@ -40,11 +47,12 @@ normalize <- function(
 normalize.default <- function(
     x,
     range = base::range(x, na.rm = TRUE),
-    bounds = 0:1
+    bounds = 0:1,
+    ...
 ) {
   x[] <- as.double(x)
-  range <- base::range(range, na.rm = TRUE)
-  bounds <- base::range(bounds, na.rm = TRUE)
+  range <- wuffle(base::range(range, na.rm = TRUE))
+  bounds <- wuffle(base::range(bounds, na.rm = TRUE))
 
   min <- range[1]
   max <- range[2]
@@ -56,8 +64,8 @@ normalize.default <- function(
   stopifnot(
     length(range) == 2,
     length(bounds) == 2,
-    min < max,
-    lower < upper
+    min <= max,
+    lower <= upper
   )
   # nocov end
 
@@ -67,11 +75,9 @@ normalize.default <- function(
 
 #' @rdname normalize
 #' @export
-normalize.data.frame <- function(
-    x,
-    range = base::range(x, na.rm = TRUE),
-    bounds = 0:1
-) {
-  x[] <- lapply(x, normalize, range = range, bounds = bounds)
+normalize.data.frame <- function(x, ...) {
+  x[] <- mapply(normalize, x = x, MoreArgs = list(...), SIMPLIFY = FALSE)
   x
 }
+
+
