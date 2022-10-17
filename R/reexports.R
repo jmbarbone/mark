92766f669d6024d7b243f0023bda7c4669bc4178
Jordan Mark Barbone <jmbarbone@gmail.com>
2022-10-16 23:53:39 -0400

    [#91] maybe actually reexport

--- R/reexports.R
+++ R/reexports.R
@@ -2,19 +2,25 @@
 # facts -------------------------------------------------------------------
 
 #' @export
-pseudo_id <- facts::pseudo_id
+#' @importFrom facts pseudo_id
+facts::pseudo_id
 
 #' @export
-fact <- facts::fact
+#' @importFrom facts fact
+facts::fact
 
 #' @export
-as_ordered <- facts::as_ordered
+#' @importFrom facts as_ordered
+facts::as_ordered
 
 #' @export
-drop_levels <- facts::drop_levels
+#' @importFrom facts drop_levels
+facts::drop_levels
 
 #' @export
-fact_na <- facts::fact_na
+#' @importFrom facts fact_na
+facts::fact_na
 
 #' @export
-fact_reverse  <- facts:::fact_reverse
+#' @importFrom facts fact_reverse
+facts::fact_reverse
