00dfd9c24ebd3d8719a62b2f3f5e4a87754cadf0
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-08-06 12:29:56 -0400

    clean up (#200)

--- R/merge-list.R
+++ R/merge-list.R
@@ -6,23 +6,18 @@
 #' @param keep When matching names are found, from which object should the values be
 #' retained; `"x"` retains values from `x`, `"y"` retains values from `y`.
 #' @examples
-#' merge_list(
-#'   list(a = 1, b = 2, c = NULL),
-#'   list(a = 2, c = 3)
-#' )
-#'
-#' merge_list(
-#'   list(a = 1, b = 2, c = NULL),
-#'   list(a = 2, c = 3),
-#'   keep = "y"
-#' )
+#' x <- list(a = 1, b = 2, c = NULL)
+#' y <- list(a = 2, c = 3)
+#' merge_list(x, y)
+#' merge_list(x, y, keep = "y")
 #' @export
 merge_list <- function(x, y, keep = c("x", "y")) {
-  keep <- match.arg(keep)
+  keep <- match_param(keep)
   x <- x %||% list()
   y <- y %||% list()
   stopifnot(is.list(x), is.list(y))
-  x <- Filter(Negate(is.null), as.list(x))
-  y <- Filter(Negate(is.null), as.list(y))
-  c(x, y)[!duplicated(c(names(x), names(y)), fromLast = keep == "y")]
+  x <- remove_null(x)
+  y <- remove_null(y)
+  res <- c(x, y)[!duplicated(c(names(x), names(y)), fromLast = keep == "y")]
+  res[order(names(res))]
 }
