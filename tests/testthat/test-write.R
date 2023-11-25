ed5d3c602ed33d5d75bb868f8e2697b41c92499f
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-11-24 21:05:21 -0500

    add other tests (#207)

--- tests/testthat/test-write.R
+++ tests/testthat/test-write.R
@@ -13,4 +13,36 @@ test_that("write_file_ms() works", {
   expect_output(write_file_ms(matrix(1:4, 2)))
 })
 
+test_that("write_file_ms() types", {
+  foo <- function(method, ...) {
+    file <- tempfile()
+    on.exit(file.remove(file))
+    df <- data.frame(a = 1)
+    expect_message(write_file_ms(df, file, method = method, ...), NA)
+  }
+
+  foo("csv")
+  foo("csv2")
+  foo("dcf")
+  foo("json")
+  foo("rds")
+  foo("table")
+  foo("tsv")
+  foo("tsv2")
+  foo("json")
+  foo("yaml")
+})
+
+test_that("write_file_ms() errors", {
+  df <- data.frame(a = 1)
+  expect_error(
+    write_file_ms(df, method = "foo"),
+    class = "matchParamMatchError"
+  )
+
+  expect_error(
+    write_file_ms(df, method = "dcf", append = TRUE)
+  )
+})
+
 # covr::file_report(covr::file_coverage("R/write.R", "tests/testthat/test-write.R"))
