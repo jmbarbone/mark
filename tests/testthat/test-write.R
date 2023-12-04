0621a23369799742e85f3b5442735ef1fdcaa997
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-12-04 00:55:28 -0500

    Update test-write.R (#224)

--- tests/testthat/test-write.R
+++ tests/testthat/test-write.R
@@ -14,15 +14,15 @@ test_that("write_file_md5() works", {
 })
 
 test_that("write_file_md5() types", {
-  foo <- function(method, ...) {
-    file <- tempfile()
+  foo <- function(method, ..., ext = "") {
+    file <- tempfile(fileext = ext)
     on.exit(file.remove(file))
-    df <- data.frame(a = 1)
+    df <- data.frame(a = 1, b = "n", c = TRUE)
     expect_message(write_file_md5(df, file, method = method, ...), NA)
   }
 
-  foo("csv")
-  foo("csv2")
+  foo("csv", ext = ".csv")
+  foo("csv2", ext = ".csv2.gz")
   foo("dcf")
   foo("json")
   foo("rds")
@@ -39,8 +39,17 @@ test_that("write_file_md5() errors", {
     write_file_md5(df, method = "foo"),
     class = "matchParamMatchError"
   )
+})
 
-  expect_error(
-    write_file_md5(df, method = "dcf", append = TRUE)
-  )
+test_that("compression works", {
+  df <- data.frame(a = 1, b = 2)
+  temp <- tempfile()
+  on.exit(fs::file_delete(temp))
+
+  muffle({
+    expect_s3_class(write_file_md5(df, temp, compress = "none"), "data.frame")
+    expect_s3_class(write_file_md5(df, temp, compress = "gz"), "data.frame")
+    expect_s3_class(write_file_md5(df, temp, compress = "bz2"), "data.frame")
+    expect_s3_class(write_file_md5(df, temp, compress = "xz"), "data.frame")
+  })
 })
