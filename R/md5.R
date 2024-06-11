ea9dce9ebdf6cccea91d147c3c5adef59366ff99
Jordan Mark Barbone <jmbarbone@gmail.com>
2024-06-10 23:42:59 -0400

    simplify md5 (#233)

--- R/md5.R
+++ R/md5.R
@@ -1,4 +1,12 @@
 #' Compute the MD5 hash of an object
+#'
+#' Wrapper for calling [tools::md5sum()] on objects rather than files.
+#'
+#' @details
+#' When `x` is a `character` vector, [base::writeLines()] is used to write to
+#' a temporary file.  Otherwise, an Rds file is created with [base::saveRds()].
+#' Coercing
+#'
 #' @param x An object
 #' @return A single `character`
 #' @export
@@ -7,24 +15,19 @@
 #' md5(1:10)
 #' md5(data.frame(a = 1:10, b = letters[1:10]))
 md5 <- function(x) {
-  UseMethod("md5")
-}
+  path <- fs::file_temp("mark_md5__")
+  file <- file(path, encoding = "UTF-8")
+  on.exit({
+    safe_close(file)
+    safe_fs_delete(path)
+  })
 
-#' @rdname md5
-#' @export
-md5.character <- function(x) {
-  do_md5(x, writeLines)
-}
+  if (is.object(x)) {
+    mark_write_rds(x, file)
+  } else {
+    x <- as.character(x)
+    mark_write_lines(x, file)
+  }
 
-#' @rdname md5
-#' @export
-md5.default <- function(x) {
-  do_md5(x, mark_write_rds)
-}
-
-do_md5 <- function(x, fun) {
-  file <- tempfile("mark_md5__")
-  on.exit(file.remove(file))
-  fun(x, file)
   unname(tools::md5sum(file))
 }
