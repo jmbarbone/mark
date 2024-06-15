50a735bae5b75547f31cf49f2cd46f8c010d5983
Jordan Mark Barbone <jmbarbone@gmail.com>
2024-06-14 21:40:53 -0400

    update tests (#233)

--- tests/testthat/_snaps/md5.md
+++ tests/testthat/_snaps/md5.md
@@ -1,7 +1,21 @@
 # snapshopts
 
     Code
-      md5(data.frame(a = 1))
+      md5(letters)
     Output
-      029415081ec344425bf6a716f5cf51bb
+      bc37452c4ba81b8d2bbb3aa6cf204c32
+
+---
+
+    Code
+      md5(1:100)
+    Output
+      0098c968f60e9718b250297103b5388b
+
+---
+
+    Code
+      md5(quick_dfl(a = 1))
+    Output
+      6311f1f7467adb01d7d97447f650b452
 
--- tests/testthat/test-md5.R
+++ tests/testthat/test-md5.R
@@ -1,11 +1,25 @@
 test_that("md5() works", {
   expect_identical(
     md5(letters),
-    "b7fdd99fac291c4bbf958d9aee731951",
+    "bc37452c4ba81b8d2bbb3aa6cf204c32",
+    ignore_attr = "class"
+  )
+
+  expect_identical(
+    md5(1:100),
+    "0098c968f60e9718b250297103b5388b",
+    ignore_attr = "class"
+  )
+
+  expect_identical(
+    md5(quick_dfl(a = 1)),
+    "6311f1f7467adb01d7d97447f650b452",
     ignore_attr = "class"
   )
 })
 
 test_that("snapshopts", {
-  expect_snapshot(md5(data.frame(a = 1)))
+  expect_snapshot(md5(letters))
+  expect_snapshot(md5(1:100))
+  expect_snapshot(md5(quick_dfl(a = 1)))
 })
