6427fb0a113cff065160ce102a6a83d6f9104232
Jordan Mark Barbone <jmbarbone@gmail.com>
2024-09-02 21:15:32 -0400

    simpify row_bind (#230)

--- R/row-bind.R
+++ R/row-bind.R
@@ -8,9 +8,16 @@
 #'   and all the columns, as they appear. An empty `data.frame` with `0` columns
 #'   and `0` rows is returned if `...` has no length
 #' @seealso [dplyr::bind_rows()] [base::rbind()]
+#' @examples
+#' row_bind(
+#'   quick_dfl(a = 1:2, b = 1:2),
+#'   quick_dfl(a = 3:4, c = 3:4),
+#'   quick_dfl(d = 5:6, b = 5:6),
+#'   quick_dfl(e = 0),
+#' )
 #' @export
 row_bind <- function(...) {
-  ls <- remove_null(if (...length() == 1) ..1 else rlang::list2(...))
+  ls <- list0(...)
 
   if (!length(ls)) {
     return(quick_df(NULL))
@@ -20,44 +27,30 @@ row_bind <- function(...) {
     stop(cond_row_bind_dataframes())
   }
 
-  names <- lapply(ls, names)
-  all_names <- unique(unlist(names))
-
-  cinds <- lapply(
-    names,
-    function(x, y) match(y, x, nomatch = NA_integer_),
-    all_names
-  )
-
-  res <- mapply(
-    function(a, b) {
-      w <- which(is.na(b))
-
-      if (length(w)) {
-        a <- quick_df(set_names(insert(a, w, NA), all_names))
-      }
-
-      a
-    },
-    a = ls,
-    b = cinds,
-    SIMPLIFY = FALSE,
-    USE.NAMES = FALSE
-  )
-
-  utils::type.convert(do.call(rbind2, res), as.is = TRUE)
-}
-
-# This may be a little faster than rbind()
-rbind2 <- function(...) {
-  ls <- rlang::list2(...)
-  res <- list()
-
-  for (i in seq_along(ls[[1]])) {
-    res[[i]] <- Reduce(c, lapply(ls, `[[`, i))
+  if (length(ls) == 1) {
+    return(ls[[1]])
   }
 
-  quick_df(set_names(res, names(ls[[1]])))
+  col_nms <- lapply(ls, names)
+  all_names <- unique(unlist(col_nms, use.names = FALSE))
+
+  res <- rep(list(NULL), length(all_names))
+  names(res) <- all_names
+
+  for (i in seq_along(ls)) {
+    cn <- col_nms[[i]]
+
+    for (col in cn) {
+      res[[col]] <- c(res[[col]], ls[[i]][[col]])
+    }
+
+    n <- nrow(ls[[i]])
+    for (col in setdiff(all_names, cn)) {
+      res[[col]] <- c(res[[col]], rep(NA, n))
+    }
+  }
+
+  mark::quick_df(res)
 }
 
 # conditions --------------------------------------------------------------
