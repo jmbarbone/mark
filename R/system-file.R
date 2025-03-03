05f88ab437c3411a5715294b83144184b9b2f46b
Jordan Mark Barbone <jmbarbone@gmail.com>
2025-03-02 22:11:35 -0500

    adds an example and return value (#129)

--- R/system-file.R
+++ R/system-file.R
@@ -1,15 +1,19 @@
 #' Make system file function
 #'
-#' Simple wrapper for package specific function for internal packages
+#' Simple wrapper for package specific function for internal packages.
 #'
 #' @param package The name of the package
+#'
+#' @returns A `function` wrapping [system.file()] which will always use the
+#' package name provided in `package`
 #' @export
-
+#' @examples
+#' make_sf("mark")()
 make_sf <- function(package) {
-  fun <- function(..., check = FALSE) { }                                                   
-  body(fun) <- substitute({
+  fun <- function(..., check = FALSE) { }
+  body(fun) <- substitute(
     match.fun("system.file")(..., package = package, mustWork = check)
-  }, environment())
+  )
   fun
 }
 
