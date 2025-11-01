0f2f81ffcc028f1f66a57cdfe80b04cc96787ac6
Jordan Mark Barbone <jmbarbone@gmail.com>
2025-10-31 20:21:46 -0400

    adds tests (#245)

--- tests/testthat/_snaps/write.md
+++ tests/testthat/_snaps/write.md
@@ -3,16 +3,20 @@
     Code
       write_file_md5(quick_dfl(a = 1), method = "feather")
     Output
-      Table
-      1 rows x 1 columns
-      $a <double>
+      <Feather file>
+      [1 x 1] @ /tmp/<temp>PT/<temp>
+      * 'a': double
 
 ---
 
     Code
       write_file_md5(quick_dfl(a = 1), method = "parquet")
     Output
-      Table
-      1 rows x 1 columns
-      $a <double>
+      # A data frame: 2 x 12
+        file_name        name  r_type type  type_length repetition_type converted_type
+        <chr>            <chr> <chr>  <chr>       <int> <chr>           <chr>         
+      1 /tmp/<temp>PT~ sche~ <NA>   <NA>           NA <NA>            <NA>          
+      2 /tmp/<temp>PT~ a     double DOUB~          NA REQUIRED        <NA>          
+      # i 5 more variables: logical_type <I<list>>, num_children <int>, scale <int>,
+      #   precision <int>, field_id <int>
 
--- tests/testthat/test-write.R
+++ tests/testthat/test-write.R
@@ -14,16 +14,17 @@ test_that("write_file_md5() works", {
 test_that("write_file_md5() types", {
   foo <- function(method) {
     temp <- withr::local_tempfile()
-    x <-
-      if (method %in% c(mark_write_methods()$lines, "write")) {
-        letters
-      } else {
-        quick_dfl(a = 1, b = "n", c = TRUE)
-      }
-      expect_message(
-        write_file_md5(x, temp, method = !!method),
-        class = fuj_message()
-      )
+    x <- if (method %in% c(mark_write_methods()$lines, "write")) {
+      letters
+    } else {
+      quick_dfl(a = 1, b = "n", c = TRUE)
+    }
+
+    # browser(expr = method == "feather")
+    expect_message(
+      write_file_md5(x, temp, method = !!method),
+      class = fuj_message()
+    )
   }
 
   for (method in unlist0(mark_write_methods())) {
@@ -108,6 +109,18 @@ test_that("list columns", {
 })
 
 test_that("arrow prints something to stdout()", {
-  expect_snapshot(write_file_md5(quick_dfl(a = 1), method = "feather"))
-  expect_snapshot(write_file_md5(quick_dfl(a = 1), method = "parquet"))
+  censor <- function(x, pattern) {
+    m <- gregexpr(pattern, x)
+    regmatches(x, m) <- "<temp>"
+    x
+  }
+
+  expect_snapshot(
+    write_file_md5(quick_dfl(a = 1), method = "feather"),
+    transform = function(x) censor(x, "(Rtmp|file)[a-z0-9]+")
+  )
+  expect_snapshot(
+    write_file_md5(quick_dfl(a = 1), method = "parquet"),
+    transform = function(x) censor(x, "(Rtmp|file)[a-z0-9]+")
+  )
 })
