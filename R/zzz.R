b432e83448169590338952315f4e0ff65bb43808
Jordan Mark Barbone <jmbarbone@gmail.com>
2026-04-11 10:40:35 -0400

    removes extra hooks (#259)

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,3 @@
-.onAttach <- function(libname, pkgname) {
-  options(op.mark[!names(op.mark) %in% names(options())])
-}
-
-.onDetach <- function(libpath) {
-  invisible()
-}
-
 # FIXME include:
 cnd_exports()
 cnd_document()
